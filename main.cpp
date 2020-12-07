81ed858433539ef2a725e9c46a676f39a89c9f0e
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2020-12-08 02:24:30 +0530

    Done with Challenges

--- main.cpp
+++ main.cpp
@@ -4574,7 +4574,7 @@ int main()
     cout<<"210. Trie Insertion and Search"<<endl;
     cout<<"211. Unique Prefix Array"<<endl;
     cout<<"212. Max Xor Pair"<<endl;
-    cout<<endl<<"******Challenges - Hashing and Tries******"<<endl;
+    cout<<endl<<"****** Challenges - Hashing and Tries ******"<<endl;
     cout<<"213. Unlock"<<endl;
     cout<<"214. Exist or Not"<<endl;
     cout<<"215. Mike And HashTrick"<<endl;
@@ -7605,7 +7605,94 @@ int main()
                         break;
                     }
         case 219 :  {
+                        /*
+                        #include <bits/stdc++.h>
+                        using namespace std ;
 
+                        class node {
+                        public :
+                            node *left ;
+                            node *right ;
+                            node(){
+                                left = right = NULL ;
+                            }
+                        };
+
+                        class Trie {
+                            node *root ;
+                        public :
+                            Trie(){
+                                root = new node() ;
+                            }
+                            void insert(int n){
+                                node *temp = root ;
+                                for(int i=31 ;i>=0;i--){
+                                    int bit = (n >> i) & 1;
+                                    if(bit == 0){
+                                        if(temp->left == NULL)
+                                            temp->left = new node() ;
+                                        temp = temp->left ;
+                                    }
+                                    else {
+                                        if(temp->right == NULL)
+                                            temp->right = new node() ;
+                                        temp = temp->right ;
+                                    }
+                                }
+                            }
+                            int findMaxXorPair(int value){
+                                node *temp = root ;
+                                int currentXor = 0 ;
+                                for(int j=31 ;j>=0 ; j--){
+                                    int bit = (value >> j) & 1;
+                                    if(bit == 0){
+                                        if(temp->right != NULL){
+                                            temp = temp->right ;
+                                            currentXor += (1<<j);
+                                        }
+                                        else
+                                            temp = temp->left ;
+                                    }
+                                    else {
+                                        if(temp->left != NULL){
+                                            temp = temp->left ;
+                                            currentXor += (1<<j);
+                                        }
+                                        else
+                                            temp = temp->right ;
+                                    }
+                                }
+                                return currentXor ;
+                            }
+                        };
+
+                        int main() {
+                            int n ;
+                            cin >> n;
+                            int result = 0, arr[n], lbest[100000], rbest[100000], left_value = 0,right_value = 0;
+
+                            Trie tl;
+                            tl.insert(0);
+                            for(int i=0;i<n;i++){
+                                cin >> arr[i] ;
+                                left_value ^= arr[i];
+                                lbest[i] = max( (i == 0)? 0 : lbest[i-1] , tl.findMaxXorPair(left_value));
+                                tl.insert(left_value);
+                            }
+
+                            Trie tr ;
+                            tr.insert(0);
+                            for(int i=n-1;i>=0;i--){
+                                right_value ^= arr[i];
+                                rbest[i] = max((i == n - 1) ? 0 : rbest[i + 1],tr.findMaxXorPair(right_value));
+                                tr.insert(right_value);
+                                int val = rbest[i] + (i == 0 ? 0 : lbest[i - 1]);
+                                result = max(result,val);
+                            }
+                            cout << result ;
+                            return 0 ;
+                        }
+                        */
                         break;
                     }
         case 220 :  {
