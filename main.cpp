063b112fc0f401f5d8a54780b38c5f8f479d5d26
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2020-12-10 21:12:44 +0530

    Working on Greedy Challenges

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include<map>
 #include<unordered_map>
 #define ll long long int
+#define MAX_SIZE 100
 using namespace std;
 typedef pair<int, pair<int,int>> node;
 
@@ -1014,6 +1015,28 @@ class TrieDict
         print(temp, s, v);
         return true;
     }
+};
+
+class HuffmanTreeNode {
+public:
+	char data;
+	int freq;
+	HuffmanTreeNode* left;
+	HuffmanTreeNode* right;
+	HuffmanTreeNode(char character,int frequency)
+	{
+		data = character;
+		freq = frequency;
+		left = right = NULL;
+	}
+};
+
+class CompareHuff {
+public:
+	bool operator()(HuffmanTreeNode* a, HuffmanTreeNode* b)
+	{
+		return a->freq > b->freq;
+	}
 };
 
 void input_array(int a[], int n)
@@ -4352,6 +4375,54 @@ bool compKnapsackGreedy(pair<int,int> p1, pair<int,int> p2)
     return pa > pb ;
 }
 
+HuffmanTreeNode* generateTree(priority_queue<HuffmanTreeNode*,vector<HuffmanTreeNode*>,CompareHuff> pq)
+{
+	while (pq.size() != 1)
+    {
+		HuffmanTreeNode* left = pq.top();
+		pq.pop();
+		HuffmanTreeNode* right = pq.top();
+		pq.pop();
+		HuffmanTreeNode* node = new HuffmanTreeNode('$', left->freq + right->freq);
+		node->left = left;
+		node->right = right;
+		pq.push(node);
+	}
+	return pq.top();
+}
+
+void printCodes(HuffmanTreeNode* root, int arr[], int top)
+{
+	if (root->left)
+    {
+		arr[top] = 0;
+		printCodes(root->left, arr, top + 1);
+	}
+	if (root->right)
+    {
+		arr[top] = 1;
+		printCodes(root->right, arr, top + 1);
+	}
+	if (!root->left && !root->right) {
+		cout << root->data << " ";
+		for (int i = 0; i < top; i++)
+			cout << arr[i];
+		cout << endl;
+	}
+}
+
+void HuffmanCodes(char data[], int freq[], int size)
+{
+	priority_queue<HuffmanTreeNode*, vector<HuffmanTreeNode*>, CompareHuff> pq;
+	for (int i = 0; i < size; i++) {
+		HuffmanTreeNode* newNode = new HuffmanTreeNode(data[i], freq[i]);
+		pq.push(newNode);
+	}
+	HuffmanTreeNode* root = generateTree(pq);
+	int arr[MAX_SIZE], top = 0;
+	printCodes(root, arr, top);
+}
+
 int main()
 {
     int ch;
@@ -4621,7 +4692,7 @@ int main()
     cout<<"226. Chopsticks"<<endl;
     cout<<"227. Expedition"<<endl;
     cout<<"228. Fractional Knapsack Greedy"<<endl;
-    cout<<"229. "<<endl;
+    cout<<"229. Huffman Coding"<<endl;
     cout<<"230. "<<endl;
     cout<<"231. "<<endl;
     cout<<"232. "<<endl;
@@ -8031,7 +8102,10 @@ int main()
                         break;
                     }
         case 229 :  {
-
+                        char data[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+                        int freq[] = { 5, 9, 12, 13, 16, 45 };
+                        int size = sizeof(data) / sizeof(data[0]);
+                        HuffmanCodes(data, freq, size);
                         break;
                     }
         case 230 :  {
